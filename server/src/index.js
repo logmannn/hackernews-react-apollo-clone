ae2256562f966639621d08cf16748f4272028936
lt <logantanous@gmail.com>
2018-08-29 18:20:01 -0500

    add authentication, connected server and database with prisma bindings

--- server/src/index.js
+++ server/src/index.js
@@ -1,73 +1,28 @@
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const AuthPayload = require("./resolvers/AuthPayload");
 const { GraphQLServer } = require("graphql-yoga");
-
-// dummy data
-let links = [
-  {
-    id: "link-0",
-    url: "www.howtographql.com",
-    description: "Fullstack tutorial for GraphQL"
-  },
-  {
-    id: "link-1",
-    url: "old.reddit.com/",
-    description: "Time wasting site"
-  }
-];
+const { Prisma } = require("prisma-binding");
 
 // 2
-let idCount = links.length;
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    // 2
-    feed: () => links,
-    link: (obj, args, context) => {
-      return links.find(link => link.id === args.id);
-    }
-  },
-  Mutation: {
-    post: async (root, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-      await links.push(link);
-      return link;
-    },
-    updateLink: async (_, { id, url, description }) => {
-      const link = await links.find(link => link.id === id);
-      if (!link["id"]) {
-        throw new Error("no message exists with id " + id);
-      }
-      if (url) {
-        link.url = url;
-      }
-      if (description) {
-        link.description = description;
-      }
-      return link;
-    },
-    deleteLink: async (_, { id }) => {
-      const link = await links.find(link => link.id === id);
-      if (!link["id"]) {
-        throw new Error("no message exists with id " + id);
-      }
-      await link.remove;
-      return link;
-    }
-  },
-  // 3
-  Link: {
-    id: root => root.id,
-    description: root => root.description,
-    url: root => root.url
-  }
+  Query,
+  Mutation,
+  AuthPayload
 };
 
 // 3
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: "src/generated/prisma.graphql",
+      endpoint: "https://us1.prisma.sh/logan-tanous/database/dev",
+      secret: "mysecret123",
+      debug: true
+    })
+  })
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
